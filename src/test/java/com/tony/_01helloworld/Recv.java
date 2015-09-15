a68696bebe794e73eb4a225735a3cb73953f0c72
tonyxu <718521330@qq.com>
2015-09-15 17:44:42 +0800

    update code

--- src/test/java/com/tony/_01helloworld/Recv.java
+++ src/test/java/com/tony/_01helloworld/Recv.java
@@ -1,35 +1,54 @@
 package com.tony._01helloworld;
 
+import java.io.IOException;
+
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConsumerCancelledException;
 import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.ShutdownSignalException;
 
 public class Recv {
 
 	private final static String QUEUE_NAME = "hello";
 
-	public static void main(String[] argv) throws Exception {
+	public static void main(String[] argv) {
 
 		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("localhost");
-		Connection connection = factory.newConnection();
-		Channel channel = connection.createChannel();
+		factory.setHost("192.168.73.128");
+		factory.setUsername("tony");
+		factory.setPassword("123");
+		try {
+			Connection connection = factory.newConnection();
+			Channel channel = connection.createChannel();
 
-		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-
-		QueueingConsumer consumer = new QueueingConsumer(channel);
-		channel.basicConsume(QUEUE_NAME, true, consumer);
-
-		while (true) {
 			/**
-			 * Server会异步发送消息，QueueingConsumer用来缓存Server发来的消息，
-			 * 在Server发送消息到QueueingConsumer之前,consumer.nextDelivery()一直是阻塞的.
+			 * 声明一个队列
 			 */
-			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
-			String message = new String(delivery.getBody(), "UTF-8");
-			System.out.println(" [x] Received '" + message + "'");
+			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+			System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+			QueueingConsumer consumer = new QueueingConsumer(channel);
+			channel.basicConsume(QUEUE_NAME, true, consumer);
+
+			while (true) {
+				/**
+				 * Server会异步发送消息，QueueingConsumer用来缓存Server发来的消息，
+				 * 在Server发送消息到QueueingConsumer之前,consumer.nextDelivery()一直是阻塞的.
+				 */
+				QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+				String message = new String(delivery.getBody(), "UTF-8");
+				System.out.println(" [x] Received '" + message + "'");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ShutdownSignalException e) {
+			e.printStackTrace();
+		} catch (ConsumerCancelledException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
 }
--- src/test/java/com/tony/_01helloworld/Send.java
+++ src/test/java/com/tony/_01helloworld/Send.java
@@ -1,37 +1,76 @@
 package com.tony._01helloworld;
 
+import java.io.IOException;
+
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+/**
+ *@Description:发送消息简单示例
+ *@Author:tony
+ *@Since:2015年9月15日
+ */
 public class Send {
 	private final static String QUEUE_NAME = "hello";
 
-	public static void main(String[] argv) throws Exception {
+	public static void main(String[] args){
 		
-//		System.out.println(argv.length);
 		ConnectionFactory factory = new ConnectionFactory();
+		
 		//RabbitMQ Server所在主机
-		factory.setHost("localhost");
-		Connection connection = factory.newConnection();
-		Channel channel = connection.createChannel();
-		
+		factory.setHost("192.168.73.128");
 		/**
-		 * 第1个参数:队列名称，只有当不存在时才会创建;
-		 * 第2个:是否创建持久队列(Server重启，队列依然存在)
-		 * 第3个:是否创建被该连接独自占有的队列
-		 * 第4个:当Server发现该队列不再被使用时，是否自动删除
-		 * 第5个:队列初始化属性
+		 * 如果是新增用户，需要在UI界面将其Can access virtual hosts置为/ ，否则报出:
+		 * java.net.SocketException: Connection reset
 		 */
-		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		factory.setUsername("tony");
+		factory.setPassword("123");
+		Connection connection = null;
+		Channel channel = null;
+		try {
+			connection = factory.newConnection();
+			channel = connection.createChannel();
+			
+			/**
+			 * 声明队列
+			 * queue: 队列名称，只有当不存在时才会创建;
+			 * durable: 是否创建持久队列(即Server重启，队列依然存在)
+			 * exclusive:该队列仅限于当前连接;
+			 * 使用exclusive的队列特点:
+			 * 1.只对首次声明它的连接可见;
+			 * 2.会在其连接断开时自动删除；
+			 * autoDelete: 当Server发现该队列不再被使用时，是否自动删除
+			 * arguments: 队列初始化属性
+			 */
+			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+			
+			String message = "Hello World!";
+			
+			/**
+			 * 发布消息
+			 * exchange为空:？
+			 * routingKey:？
+			 * BasicProperties:?
+			 * body:消息内容byte数组
+			 */
+			channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+			System.out.println(" [x] Sent '" + message + "'");
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				if(channel != null){
+					channel.close();
+				}
+				if(connection != null){
+					connection.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 		
-		String message = "Hello World!";
-		
-		//消息内容是byte数组
-		channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
-		System.out.println(" [x] Sent '" + message + "'");
-		
-		channel.close();
-		connection.close();
 	}
 }
